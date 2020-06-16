1844aa444aedd8c3f8b84e448f3cc32606ba1058
Damon Schulz <damon.schulz@gmail.com>
2020-06-16 00:47:30 -0500

    Updated Randomization of Trip Outcomes

--- daytripgen.js
+++ daytripgen.js
@@ -12,6 +12,63 @@ function getRandomNumber(array) {
     return randomSelection;
 }
 
-console.log(getRandomNumber(destinations))
-console.log(getRandomNumber(methodsOfTranspo))
-console.log(getRandomNumber(typesOfEntertainment))
+// console.log(getRandomNumber(destinations))
+// console.log(getRandomNumber(methodsOfTranspo))
+// console.log(getRandomNumber(typesOfEntertainment))
+
+function getRandomNumber(array){ 
+    let randomSelect =  array[Math.floor(Math.random() * array.length)]; 
+    return randomSelect;
+}
+
+getRandomNumber(destinations);
+getRandomNumber(methodsOfTranspo);
+getRandomNumber(typesOfEntertainment);
+
+
+
+function run(){
+    let userInput = prompt("Would you like for us to plan you a vacation?");
+        if(userInput === "yes"){
+            console.log("You will go to " + getRandomNumber(destinations))
+            console.log("You will travel there by " + getRandomNumber(methodsOfTranspo))
+            console.log("While you are there, you will spend lots of time " + getRandomNumber(typesOfEntertainment))
+        }
+
+    
+}
+
+run();
+
+
+
+
+
+//     while (userInput !== properResponse){
+//         userInput = prompt("Please enter a new response.");
+//     }
+//         if (userInput == properResponse){
+//              (getRandomNumber(destinations))
+//              (getRandomNumber(methodsOfTranspo))
+//              (getRandomNumber(typesOfEntertainment))   
+        
+//         return true;
+//         }
+// }
+
+// run();
+
+
+// run();
+//     if(userInput == "no") {
+//         console.log("Sorry, you must find a new business to help you.")
+    
+//     } 
+   
+//     else {
+//         console.log("Your destination will be " + getRandomNumber(destination)) , "You will arrive there by way of " + (getRandomNumber(methodsOfTranspo)) ,
+//         "and while you are there, you will spend lots of time " + (getRandomNumber(typesOfEntertainment) +  ".")
+//         }
+// }
+
+// run()
