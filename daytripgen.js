257966ed6474c9d4d1efa510aee1e27c7e43af70
Damon Schulz <damon.schulz@gmail.com>
2020-06-16 12:36:47 -0500

    Added verification for user-entered trip

--- daytripgen.js
+++ daytripgen.js
@@ -5,26 +5,12 @@ let methodsOfTranspo = ["Plane" , "Car" , "Train" , "Hitchhike"];
 let typesOfEntertainment = ["Seeing a Movie" , "Swimming" , "Birdwatching" , "Crying"];
 let restaurant = ["Whataburger" , "Olive Garden" , "The Cheesecake Factory" , "Chik-fil-a"];
 
-function getRandomNumber(array) {
-    let myArray = destinations || methodsOfTranspo || typesOfEntertainment;
-    let randomItem = [Math.floor(Math.random()* myArray.length)];
-    let randomSelection = parseFloat(randomItem);
-    return randomSelection;
-}
 
-// console.log(getRandomNumber(destinations))
-// console.log(getRandomNumber(methodsOfTranspo))
-// console.log(getRandomNumber(typesOfEntertainment))
 
 function getRandomNumber(array){ 
     let randomSelect =  array[Math.floor(Math.random() * array.length)]; 
     return randomSelect;
 }
-
-getRandomNumber(destinations);
-getRandomNumber(methodsOfTranspo);
-getRandomNumber(typesOfEntertainment);
-getRandomNumber(restaurant);
 
 
 function run(){
@@ -43,19 +29,26 @@ function run(){
                         if (secondChance == "yes"){
                      
                             run();
-             
+                }       
                         
-                    }
-                }
+                    
+                        }
 
                 
              } else{
            let usersChoice = prompt("Let us know where you want to go, hpw you want to get there, and what you want to do while you are there.")
-           console.log(usersChoice);
-        }
+           let verify = prompt("Are you happy with this vacation?")
+                if(verify == "yes"){
+                    console.log("Here is your itinerary " + usersChoice);
+                }else{
+                    let newTrip = prompt("Enter new vacation details")
+                        console.log("Here is your updated itinerary " + newTrip);
+                    }
+                }
+            }
         
 
-}
+
 
 run();
 
@@ -63,31 +56,4 @@ run();
 
 
 
-//     while (userInput !== properResponse){
-//         userInput = prompt("Please enter a new response.");
-//     }
-//         if (userInput == properResponse){
-//              (getRandomNumber(destinations))
-//              (getRandomNumber(methodsOfTranspo))
-//              (getRandomNumber(typesOfEntertainment))   
-        
-//         return true;
-//         }
-// }
 
-// run();
-
-
-// run();
-//     if(userInput == "no") {
-//         console.log("Sorry, you must find a new business to help you.")
-    
-//     } 
-   
-//     else {
-//         console.log("Your destination will be " + getRandomNumber(destination)) , "You will arrive there by way of " + (getRandomNumber(methodsOfTranspo)) ,
-//         "and while you are there, you will spend lots of time " + (getRandomNumber(typesOfEntertainment) +  ".")
-//         }
-// }
-
-// run()
