c7ee7f9a64f8b6c2828e089a7707dda0283bf770
Damon Schulz <damon.schulz@gmail.com>
2020-06-16 11:59:18 -0500

    Finnished Loop for Verification of trip choices

--- daytripgen.js
+++ daytripgen.js
@@ -28,18 +28,33 @@ getRandomNumber(restaurant);
 
 
 function run(){
+    
     let userInput = prompt("Would you like for us to plan you a vacation?");
         if(userInput === "yes"){
             console.log("You will go to " + getRandomNumber(destinations))
             console.log("You will travel there by " + getRandomNumber(methodsOfTranspo))
             console.log("While you are there, you will spend lots of time " + getRandomNumber(typesOfEntertainment))
-            console.log("We recommend " + getRandomNumber(restaurant) + " as a great place to eat.")
-        }else{
+            console.log("We recommend " + getRandomNumber(restaurant) + " as a great place to eat.");
+            let userSatisfied = prompt("Are you satisfied with these choices?")
+                if(userSatisfied == "yes"){
+                    console.log( "You are all set for your trip!");
+                }else{
+                    let secondChance =prompt("Would you like a new random selection")
+                        if (secondChance == "yes"){
+                     
+                            run();
+             
+                        
+                    }
+                }
+
+                
+             } else{
            let usersChoice = prompt("Let us know where you want to go, hpw you want to get there, and what you want to do while you are there.")
            console.log(usersChoice);
         }
+        
 
-    
 }
 
 run();
